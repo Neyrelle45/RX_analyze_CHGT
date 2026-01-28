d9e7d71bf53c01d774b8d2bc5311bf5656da4730
Neyrelle45 <pierre.kieffer45@laposte.net>
2026-01-28 17:53:48 +0100

    Update model.py

--- engine/model.py
+++ engine/model.py
@@ -1,18 +1,56 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-class UNetLite(nn.Module):
+class DoubleConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.seq = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.seq(x)
+
+class UNet(nn.Module):
     def __init__(self, n_classes=3):
         super().__init__()
-        self.enc1 = nn.Sequential(nn.Conv2d(1,16,3,padding=1), nn.ReLU())
-        self.enc2 = nn.Sequential(nn.Conv2d(16,32,3,padding=1), nn.ReLU())
-        self.pool = nn.MaxPool2d(2)
-        self.dec1 = nn.Sequential(nn.Conv2d(32,16,3,padding=1), nn.ReLU())
-        self.out  = nn.Conv2d(16,n_classes,1)
 
-    def forward(self,x):
-        e1 = self.enc1(x)
-        e2 = self.enc2(self.pool(e1))
-        d1 = F.interpolate(e2, scale_factor=2)
-        d1 = self.dec1(d1)
-        return self.out(d1)
+        self.d1 = DoubleConv(1, 32)
+        self.d2 = DoubleConv(32, 64)
+        self.d3 = DoubleConv(64, 128)
+        self.d4 = DoubleConv(128, 256)
+
+        self.pool = nn.MaxPool2d(2)
+
+        self.u3 = nn.ConvTranspose2d(256, 128, 2, stride=2)
+        self.c3 = DoubleConv(256, 128)
+
+        self.u2 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+        self.c2 = DoubleConv(128, 64)
+
+        self.u1 = nn.ConvTranspose2d(64, 32, 2, stride=2)
+        self.c1 = DoubleConv(64, 32)
+
+        self.out = nn.Conv2d(32, n_classes, 1)
+
+    def forward(self, x):
+        c1 = self.d1(x)
+        c2 = self.d2(self.pool(c1))
+        c3 = self.d3(self.pool(c2))
+        c4 = self.d4(self.pool(c3))
+
+        x = self.u3(c4)
+        x = self.c3(torch.cat([x, c3], dim=1))
+
+        x = self.u2(x)
+        x = self.c2(torch.cat([x, c2], dim=1))
+
+        x = self.u1(x)
+        x = self.c1(torch.cat([x, c1], dim=1))
+
+        return self.out(x)
