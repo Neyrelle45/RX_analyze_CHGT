d61d63f81ed6c56747fc3ec8311e63cf1c31f0cc
Neyrelle45 <pierre.kieffer45@laposte.net>
2026-01-29 10:25:45 +0100

    Update preprocessing.py

--- engine/preprocessing.py
+++ engine/preprocessing.py
@@ -1,7 +1,14 @@
 import cv2
 
+TARGET_SIZE = 512
+
 def preprocess_rx(img, contrast=1.0, denoise=5):
+
+    img = cv2.resize(img, (TARGET_SIZE, TARGET_SIZE))
+
     img = cv2.convertScaleAbs(img, alpha=contrast)
+
     if denoise > 0:
         img = cv2.fastNlMeansDenoising(img, None, denoise)
+
     return img
